dc50d4a43683860b9336f3bce26bf58974b3ed28
לוסטיג מוריה < @mbjcomp.local>
2025-04-21 17:45:06 +0300

    adding the project to github

---
+++ Dal/Models/Doctor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dal.models;
+
+public partial class Doctor
+{
+    public string Id { get; set; } = null!;
+
+    public string? FirstName { get; set; }
+
+    public string Lastname { get; set; } = null!;
+
+    public string PhoneNumber { get; set; } = null!;
+
+    public double SalaryPerHour { get; set; }
+
+    public string Specialization { get; set; } = null!;
+
+    public string WorkingDays { get; set; } = null!;
+
+    public string WorkingHours { get; set; } = null!;
+}
---
+++ Dal/Models/Patient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dal.models;
+
+public partial class Patient
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string PhoneNumber { get; set; } = null!;
+
+    public int Age { get; set; }
+
+    public int Weight { get; set; }
+
+    public int MedicalBag { get; set; }
+
+    public virtual Meeting MedicalBagNavigation { get; set; } = null!;
+}
