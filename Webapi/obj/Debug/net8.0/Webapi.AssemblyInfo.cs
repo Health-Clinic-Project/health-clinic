c8868922b43c1e9faa91b9d500e06c8b67d39234
DassiOzer <Hand3D25@mbjcomp.org.il>
2025-04-21 17:54:52 +0300

    change

--- Webapi/obj/Debug/net8.0/Webapi.AssemblyInfo.cs
+++ Webapi/obj/Debug/net8.0/Webapi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Webapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05b7a95a1666a7c292c096e2e8c6c104bb0b5077")]
 [assembly: System.Reflection.AssemblyProductAttribute("Webapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Webapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
